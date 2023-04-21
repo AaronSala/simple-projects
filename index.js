857d2caf9d3ae36366d81fccb237e87f6b3eb468
AaronSala <115704695+AaronSala@users.noreply.github.com>
2023-04-21 12:53:53 +0300

    Add files via upload
    nav bar

---
+++ index.js
@@ -0,0 +1,14 @@
+//const nav = document.querySelector('.nav-header')
+var btn = document.querySelector('.btn')
+//var header = document.getElementById("header")
+
+btn.addEventListener('click', ()=>{
+    console.log('clicked')
+    header.style.display = "block";
+    header.style.backgroundColor = "red";
+
+})
+
+   
+    
+ 
